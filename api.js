09eb56e81976dcb788175efcc1bf0b62ca7438d9
slfubu <thapanutputtathakorn@gmail.com>
2026-07-08 10:56:17 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz12gLInHJqme8YB9b29eZKn81XoDjkPpEfHyYXKkAOS3dw6f_d8NnexTKDbIUGQs08/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzj5NksvbbCIMs8g_9gxsEIrTE25rsb6Sg9rQI5rSPSyl-3OY9MYWvr_ct1a5RkATvh/exec";
 
 async function callApi(action, payload) {
     const loader = document.getElementById('customLoader');
